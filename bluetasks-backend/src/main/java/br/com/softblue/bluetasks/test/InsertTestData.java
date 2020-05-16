8e111cb629c0df4e2ac7cc2eeca39cc3960fe0d4
anderson.oliveirasilva@gmail.com <andersonosprojs>
2020-05-16 10:58:12 -0300

    versão #1  - Criação do projeto  - Criação do H2 database  - Validações  - Tratamento do retorno de erros das validações

---
+++ bluetasks-backend/src/main/java/br/com/softblue/bluetasks/test/InsertTestData.java
@@ -0,0 +1,42 @@
+package br.com.softblue.bluetasks.test;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import br.com.softblue.bluetasks.domain.task.Task;
+import br.com.softblue.bluetasks.domain.task.TaskRepository;
+import br.com.softblue.bluetasks.domain.user.AppUser;
+import br.com.softblue.bluetasks.domain.user.AppUserRepository;
+
+@Component
+public class InsertTestData {
+	
+	private TaskRepository taskRepository;
+	private AppUserRepository appUSerRepository;
+	
+	@Autowired
+	public InsertTestData(TaskRepository taskRepository, AppUserRepository appUSerRepository) {
+		this.taskRepository = taskRepository;
+		this.appUSerRepository = appUSerRepository;
+	}
+	
+	@EventListener
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		//TODO: Criptografar senha
+		AppUser appUser = new AppUser("john", "abc", "John Coder");
+		appUSerRepository.save(appUser);	
+	
+		LocalDate baseDate = LocalDate.parse("2025-02-01");
+		
+		for (int i = 1; i <= 10; i++) {
+			Task task = new Task("Tarefa #" + i, baseDate.plusDays(i), false);
+			task.setAppUser(appUser);
+			taskRepository.save(task);
+		}
+		
+	}
+}
