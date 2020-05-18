ae539f7821b9f6d2f6f84f226b4f1b470877a8ef
anderson.oliveirasilva@gmail.com <andersonosprojs>
2020-05-18 14:26:40 -0300

    versão #1.2
     - Implementação de toda estrutura de segurança

--- bluetasks-backend/src/main/java/br/com/softblue/bluetasks/test/InsertTestData.java
+++ bluetasks-backend/src/main/java/br/com/softblue/bluetasks/test/InsertTestData.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import br.com.softblue.bluetasks.domain.task.Task;
@@ -26,8 +28,10 @@ public class InsertTestData {
 	
 	@EventListener
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		//TODO: Criptografar senha
-		AppUser appUser = new AppUser("john", "abc", "John Coder");
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(); 
+				
+		AppUser appUser = new AppUser("john", encoder.encode("abc"), "John Coder");
 		appUSerRepository.save(appUser);	
 	
 		LocalDate baseDate = LocalDate.parse("2025-02-01");
